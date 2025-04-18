2dbc3298c7ebb139e8153f104ca4f05c4276180f
Jacob Seatlholo <66915934+JacobSeatlholo@users.noreply.github.com>
2025-04-18 21:42:35 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,3 @@ import 'nextra-theme-docs/style.css'
 export default function Nextra({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
-
-<script async src="https://www.googletagmanager.com/gtag/js?id=AW-786327701">
-</script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'AW-786327701');
-</script>
