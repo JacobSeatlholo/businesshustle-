dfd737e610b3d1deee5fb02d42d80345030b5613
Jacob Seatlholo <seatlholojacob@gmail.com>
2026-06-24 17:14:07 +0200

    add facebook domain verification meta tag

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,14 @@
 
 import 'nextra-theme-docs/style.css';
 import Script from 'next/script';
+import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta name="facebook-domain-verification" content="u43o0mbmp0qo80lvlgf2oxcrfafmr5" />
+      </Head>
       {/* Google tag (gtag.js) */}
       <Script
         strategy="afterInteractive"
@@ -18,7 +22,6 @@ export default function MyApp({ Component, pageProps }) {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-
             gtag('config', 'AW-786327701');
           `,
         }}
